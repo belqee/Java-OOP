539ed04e9280f9173bf0538b9988ca4598eb737e
Mihail Zyryanov <sweeqty@gmail.com>
2024-02-28 17:56:20 +0700

    lab1

---
+++ lab1/src/main/java/org/nsu/oop/task1/App.java
@@ -0,0 +1,11 @@
+package org.nsu.oop.task1;
+import org.nsu.oop.task1.BullsAndCowsGame;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        BullsAndCowsGame game = new BullsAndCowsGame();
+        game.start();
+    }
+}
